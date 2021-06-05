305a317a0d5823a987f77bc8a04f5db67fb5bb69
Vsevolod <2007juggernaut@gmail.com>
2021-06-05 17:19:50 +0300

    Allow to create employees with new company

--- WebApplication/Models/CompanyForCreationDto.cs
+++ WebApplication/Models/CompanyForCreationDto.cs
@@ -1,9 +1,12 @@
-﻿namespace WebApplication.Models
+﻿using System.Collections.Generic;
+
+namespace WebApplication.Models
 {
     public class CompanyForCreationDto
     {
         public string Name { get; set; }
         public string Address { get; set; }
         public string Country { get; set; }
+        public IEnumerable<Employee> Employees { get; set; }
     }
 }
